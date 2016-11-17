50daa758db6453ae632349d21ab9b571739af948
Elias <elainsa@insa.gov.et>
2016-11-17 09:47:17 +0300

    adding server  and main class

---
+++ eclipseGithubT/src/TestGitH/Main.java
@@ -0,0 +1,13 @@
+package TestGitH;
+
+public class Main {
+	public static void main(String [] args){
+		
+		Sum add=new Sum();
+		
+	}
+	
+	
+	
+
+}
---
+++ eclipseGithubT/src/TestGitH/Sum.java
@@ -0,0 +1,12 @@
+package TestGitH;
+
+public class Sum {
+
+	int num1,num2;
+	String name;
+	public Sum(){
+		name="Elias";
+		num1=10;
+		num2=60;
+	}
+}
